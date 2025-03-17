e12652188d28d053fc31404c2cb9d1f0908587d0
IJurij <yjurij@gmail.com>
2025-03-17 19:11:43 +0300

    added offfers, comments buttons to forms

--- app/UI/Admin/CMS/Offers/Comments/CommentsPresenter.php
+++ app/UI/Admin/CMS/Offers/Comments/CommentsPresenter.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace App\UI\Admin\CMS\Offers\Comments;
 
@@ -8,8 +8,14 @@ final class CommentsPresenter extends \App\UI\Admin\BasePresenter
 {
     public function renderDefault()
     {
-        if (!$this->getUser()->isAllowed('Comments', '')) {
+        if (! $this->getUser()->isAllowed('Comments', '')) {
             $this->error('Forbidden', 403);
         }
+    }
+
+    #[Requires(methods: 'POST', sameOrigin: true)]
+    public function renderByClient(int $id): void
+    {
+
     }
 }
--- app/UI/Admin/CMS/Offers/OffersPresenter.php
+++ app/UI/Admin/CMS/Offers/OffersPresenter.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace App\UI\Admin\CMS\Offers;
 
@@ -8,8 +8,14 @@ final class OffersPresenter extends \App\UI\Admin\BasePresenter
 {
     public function renderDefault()
     {
-        if (!$this->getUser()->isAllowed('Orders', '')) {
+        if (! $this->getUser()->isAllowed('Orders', '')) {
             $this->error('Forbidden', 403);
         }
+    }
+
+    #[Requires(methods: 'POST', sameOrigin: true)]
+    public function renderByClient(int $id): void
+    {
+
     }
 }
