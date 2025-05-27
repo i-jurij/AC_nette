212ac79f5dea955a4bd9dd1790f62d66e0d298d9
I-Jurij <yjurij@gmail.com>
2025-05-27 21:12:14 +0300

    ср

--- app/Model/GrievanceFacade.php
+++ app/Model/GrievanceFacade.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use Nette\Utils\DateTime;
+
 use App\UI\Accessory\RequireLoggedUser;
 // use Nette\Database\Connection;
 use Nette\Database\Explorer;
@@ -21,10 +23,42 @@ class GrievanceFacade
         $this->gr = $this->db->table('grievance');
     }
 
-    public function getNotResolve()
+    public function getNotResolve(): array
     {
         return $this->gr->where('resolve', false)->fetchAll();
     }
+
+    public function getResolved(): array
+    {
+        return $this->gr->where('resolve', true)->fetchAll();
+    }
+
+    public function getByClient(int $id): array
+    {
+        return $this->gr
+            ->where('resolve', false)
+            ->where('client_id_who', $id)
+            ->fetchAll();
+    }
+
+    public function markResolved(int $id): int
+    {
+        return $this->gr->where('id', $id)->update([
+            'resolve' => 1,
+            'resolve_time' => new DateTime,
+        ]);
+
+    }
+
+    public function removeOld(): int
+    {
+        $original = new DateTime;
+        $one_year_ago = $original->modifyClone('-1 year');
+        return $this->gr
+            ->where('resolve_time <', $one_year_ago)
+            ->delete();
+    }
+
     public function create(array $data): int
     {
         $row = $this->gr->insert($data);
@@ -38,4 +72,6 @@ class GrievanceFacade
     public function delete()
     {
     }
+
+
 }
--- app/UI/Admin/CMS/Grievance/GrievancePresenter.php
+++ app/UI/Admin/CMS/Grievance/GrievancePresenter.php
@@ -21,10 +21,42 @@ final class GrievancePresenter extends \App\UI\Admin\BasePresenter
         $this->template->grievance_not_resolve = $this->gf->getNotResolve();
     }
 
-    public function renderByClient(int $id)
+    public function renderResolved()
     {
         if (!$this->getUser()->isAllowed('Grievance', '')) {
             $this->error('Forbidden', 403);
         }
+        $this->template->grievance = $this->gf->getResolved();
+    }
+
+    public function handleMarkResolved(int $id)
+    {
+        if (!$this->getUser()->isAllowed('Grievance', 'markResolved')) {
+            $this->error('Forbidden', 403);
+        }
+        if ($this->gf->markResolved($id)) {
+            $this->flashMessage('Grievance mark as resolved', 'success');
+        }
+        $this->redirect('this');
+    }
+
+    public function handleRemoveOld()
+    {
+        if (!$this->getUser()->isAllowed('Grievance', 'removeOld')) {
+            $this->error('Forbidden', 403);
+        }
+        if ($this->gf->removeOld()) {
+            $this->flashMessage('Old grievance is deleted', 'success');
+        }
+        $this->redirect('this');
+    }
+
+    public function renderByClient(int $id, string $username)
+    {
+        if (!$this->getUser()->isAllowed('Grievance', '')) {
+            $this->error('Forbidden', 403);
+        }
+        $this->template->grievance = $this->gf->getByClient($id);
+        $this->template->client_name = htmlspecialchars(strip_tags($username));
     }
 }
