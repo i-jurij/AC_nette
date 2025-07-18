b44c09913c89d1777a2492c643f9332397faf1f5
I-Jurij <yjurij@gmail.com>
2025-07-18 19:18:09 +0300

    ch

--- www/index.php
+++ www/index.php
@@ -1,9 +1,8 @@
 <?php
-
 declare(strict_types=1);
 
 // require __DIR__ . '/../vendor/autoload.php';
-if (@!include __DIR__.'/../vendor/autoload.php') {
+if (@!include __DIR__ . '/../vendor/autoload.php') {
     exit('Install Nette using `composer update`');
 }
 
