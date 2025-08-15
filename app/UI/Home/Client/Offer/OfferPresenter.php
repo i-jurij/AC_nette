af3039ca5d3ea87311e7319d8f80400b629852d0
I-Jurij <yjurij@gmail.com>
2025-08-15 19:29:26 +0300

    ch

--- app/UI/Home/Client/Offer/OfferPresenter.php
+++ app/UI/Home/Client/Offer/OfferPresenter.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\UI\Home\Client\Offer;
 
 use App\Model\OfferFacade;
--- app/UI/Home/Offer/OfferPresenter.php
+++ app/UI/Home/Offer/OfferPresenter.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\UI\Home\Offer;
 
 use \App\Model\ChatFacade;
